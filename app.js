d8365c47af14c1610585fd9925601e822d4ad42d
Samkiroko <kirokodev@gmail.com>
2020-10-28 13:08:21 +0300

    adding js and css transform properties

--- app.js
+++ app.js
@@ -8,3 +8,14 @@ container.addEventListener("mousemove", (e) => {
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
+
+// Animate in
+container.addEventListener('mouseenter', (e) =>{
+  card.style.transition = 'none';
+})
+
+// Animate out
+container.addEventListener('mouseleave', (e)=>{
+  card.style.transition = 'all 0.5s ease';
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
